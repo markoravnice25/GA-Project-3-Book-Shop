d31ca74ed7734249d57721e89d09b66c4d5a32ec
Marko Filipovic <markoravnice25@gmail.com>
2022-05-17 16:58:30 +0200

    added authentication to loginUser in auth.js

--- config/environment.js
+++ config/environment.js
@@ -1,2 +1,3 @@
 export const PORT = 3000
 export const MONGODB_CONNECTION_STRING = 'mongodb://127.0.0.1:27017/books'
+export const SECRET = '-.7CqRCwd{*aT8~'
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 import User from '../models/users.js'
 import jwt from 'jsonwebtoken'
-import { PORT, MONGODB_CONNECTION_STRING } from '../config/environment.js'
+import { SECRET } from '../config/environment.js'
 
 // METHOD: POST
 // Endpoint: /register
@@ -20,10 +20,18 @@ export const loginUser = async (req, res) => {
   const { body } = req
   try {
     console.log('user body ->', body)
-    const { email } = body
+    const { email, password } = body
     const userToLogin = await User.findOne({ email: email })
     console.log('user to loin -> ', userToLogin)
-    return res.status(200).json({ message: `Welcome back ${userToLogin.username}` })
+
+    if (!userToLogin || !userToLogin.validatePassword(password)){
+      throw new Error()
+    }
+
+    const token = jwt.sign({ sub: userToLogin._id }, SECRET, { expiresIn: '6h' })
+    console.log('token -> ', token)
+
+    return res.status(200).json({ message: `Welcome back ${userToLogin.username}`, token: token })
   } catch (error) {
     console.log(error)
     return res.status(422).json({ message: 'unauthorised champ' })
