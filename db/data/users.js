ccff233fa6d181ce7d3523152d98a7489a699852
Marko Filipovic <markoravnice25@gmail.com>
2022-05-20 13:30:48 +0200

    userSchema added property types to be in line with our webpage, and added that to /data/users.js

--- db/data/users.js
+++ db/data/users.js
@@ -1,20 +1,26 @@
 export default [
   {
-    username: 'Marko',
+    firstName: 'marko',
+    lastName: 'marko',
     email: 'marko@marko.com',
+    confirmEmail: 'marko@marko.com',
     password: 'marko',
-    passwordConfirmation: 'marko',
+    confirmPassword: 'marko',
   },
   {
-    username: 'Daisy',
+    firstName: 'daisy',
+    lastName: 'daisy',
     email: 'daisy@daisy.com',
+    confirmEmail: 'daisy@daisy.com',
     password: 'daisy',
-    passwordConfirmation: 'daisy',
+    confirmPassword: 'daisy',
   },
   {
-    username: 'Riccardo',
+    firstName: 'riccardo',
+    lastName: 'riccardo',
     email: 'riccardo@riccardo.com',
+    confirmEmail: 'riccardo@riccardo.com',
     password: 'riccardo',
-    passwordConfirmation: 'riccardo',
+    confirmPassword: 'riccardo',
   }
 ]
