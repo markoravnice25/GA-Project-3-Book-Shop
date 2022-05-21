b4b43c5320a255600d09dbdec031e0fc64be5f3a
Marko Filipovic <markoravnice25@gmail.com>
2022-05-21 21:30:31 +0200

    basic Login.js is back

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,7 @@ import Home from './components/Home.js'
 
 import BookShow from './components/BookShow.js'
 import Register from './components/auth/Register.js'
-// import Login from './components/auth/Login.js'
-
+import Login from './components/auth/Login.js'
 // import Whishlist from './components/Whishlist.js'
 // import Account from './components/Account.js'
 import NotFound from './common/NotFound.js'
@@ -33,12 +32,12 @@ const App = () => {
         <Route path="/books/:id/#write-review" element={<BookShow />} /> */}
         <Route path="/register" element={<Register />} />
 
-        {/* <Route path="/login" element={<Login />} />     
-         <Route path="/account" element={<Account />} />    
+        <Route path="/login" element={<Login />} />     
+        {/* <Route path="/account" element={<Account />} />    
         <Route path="/account/whishlist" element={<Whishlist />} />   */}
 
         <Route path="*" element={<NotFound />} /> 
- 
+
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,7 @@
+const Login = () => {
+  return (
+    <h1>This is the Login page Baby!</h1>
+  )
+}
+
+export default Login
