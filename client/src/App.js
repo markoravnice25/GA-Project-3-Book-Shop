12d1840d11a7691ac7699293e727196d29286260
Marko Filipovic <markoravnice25@gmail.com>
2022-05-21 21:20:52 +0200

    adding modal to Login.js; adding basic display to WishList.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,8 @@ import Home from './components/Home.js'
 
 import BookShow from './components/BookShow.js'
 import Register from './components/auth/Register.js'
-// import Login from './components/auth/Login.js'
-
-// import Whishlist from './components/Whishlist.js'
+import Login from './components/auth/Login.js'
+import Whishlist from './components/Whishlist.js'
 // import Account from './components/Account.js'
 import NotFound from './common/NotFound.js'
 import Reviews from './components/Reviews.js'
@@ -33,9 +32,9 @@ const App = () => {
         <Route path="/books/:id/#write-review" element={<BookShow />} /> */}
         <Route path="/register" element={<Register />} />
 
-        {/* <Route path="/login" element={<Login />} />     
-         <Route path="/account" element={<Account />} />    
-        <Route path="/account/whishlist" element={<Whishlist />} />   */}
+        <Route path="/login" element={<Login />} />     
+        {/* <Route path="/account" element={<Account />} /> */}
+        <Route path="/account/whishlist" element={<Whishlist />} />
 
         <Route path="*" element={<NotFound />} /> 
  
