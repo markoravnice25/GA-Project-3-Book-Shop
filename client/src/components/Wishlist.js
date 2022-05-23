5c5c1f350aa07723351a3459b5cadedcabb640a7
Marko Filipovic <markoravnice25@gmail.com>
2022-05-23 11:06:51 +0200

    created ternary in WishList.js

--- client/src/components/Wishlist.js
+++ client/src/components/Wishlist.js
@@ -1,49 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
 import Container from 'react-bootstrap/esm/Container'
 import Row from 'react-bootstrap/esm/Row'
 import Col from 'react-bootstrap/esm/Col'
 
 const WishList = () => {
 
-  const [books, setBooks] = useState([])
+  const [ books, setBooks ] = useState([])
   console.log('wishlist check')
 
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get('/api/books/') // * <-- replace with your endpoint
-      setBooks(data)
+      try {
+        const response = await axios.get('/api/books')
+        console.log(response.data)
+        setBooks(response.data)
+      } catch (error) {
+        console.log(error)
+      }
     }
     getData()
   }, [])
   console.log(books)
-  console.log('wishlist check 2')
 
   return (
     <section>
       <h1>My Wishlist!</h1>
       <p>A wishlist by -------</p>
-      <Container className='wishlist-all'>
-        <hr />
-        <Row className='wishlist-one'>
-          <Col md='4' sm='6'>
-            <img src={books[0].image} alt={books[0].title} />
-          </Col>
-          <Col md='8' sm='6'>
-            <h4>{books[0].title}</h4>
-            <h5>{books[0].author}</h5>
-            <h4>￡{books[0].price}</h4>
-            <h5>{books[0].genre}</h5>
-            <p>{books[0].description}</p>
-          </Col>
-        </Row>
-        <hr />
-      </Container>
+      { books && books[0] ?
+        <Container className='wishlist-all'>
+          <hr />
+          <Row className='wishlist-one'>
+            <Col md='4' sm='6'>
+              <img src={books[0].image} alt={books[0].title} />
+            </Col>
+            <Col md='8' sm='6'>
+              <h4>{books[0].title}</h4>
+              <h5>{books[0].author}</h5>
+              <h4>￡{books[0].price}</h4>
+              <h5>{books[0].genre}</h5>
+              <p>{books[0].description}</p>
+            </Col>
+          </Row>
+          <hr />
+        </Container>
+        :
+        <h1>No items in WishList yet!</h1>
+      }
     </section>
-
   )
 }
 
