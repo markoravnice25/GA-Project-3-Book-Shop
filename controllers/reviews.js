08989ec6b8a90289aba27ed8870288cfc1f96049
riccardo <riccardogiubiloweb@gmail.com>
2022-05-24 16:32:47 +0200

    fixing reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -18,20 +18,28 @@ export const getReviews = async (req, res) => {
 
 export const deleteReview = async (req, res) => {
   const { id, reviewId } = req.params
-
+  console.log('id --->', id)
+  console.log('req --->', req.params)
   try {
   
     const reviewToDelete = await Review.findById(reviewId)
-    const book = await Book.findOne(id)
-    const reviewToDeleteFromBook = book.reviews.id(reviewId)
-    console.log('review to delete from book again --->,', reviewToDeleteFromBook)
-    console.log('review Id --->', reviewId)
-    if (!reviewToDelete) throw new Error('Review not found')
-    
-    await reviewToDelete.remove()
-    await reviewToDeleteFromBook.remove()
+    console.log('reviewToDelete --->', reviewToDelete)
 
-    await book.save()
+    const book = await Book.find( { 'reviews._id': reviewId })
+    console.log('the book --->', book)
+
+    const reviewToDeleteFromBook = book.reviews.id(reviewId)
+    if (!reviewToDeleteFromBook) throw new Error('Review not found')
+    
+    console.log('review to delete from book again --->,', reviewToDeleteFromBook)
+
+    // console.log('review Id --->', reviewId)
+    // if (!reviewToDelete) throw new Error('Review not found')
+    
+    // await reviewToDelete.remove()
+    // await reviewToDeleteFromBook.remove()
+
+    // await book.save()
     return res.sendStatus(204)
   } catch (error) {
     console.log(error)
