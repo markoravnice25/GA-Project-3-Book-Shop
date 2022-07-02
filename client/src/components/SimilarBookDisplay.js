4543e38cfb2f0d8be6a9c5b764339b037fe8520a
markoravnice25 <markoravnice25@gmail.com>
2022-07-02 20:36:07 +0200

    deleted repeat book details on SimilarBookDisplay.js and refactored code

--- client/src/components/SimilarBookDisplay.js
+++ client/src/components/SimilarBookDisplay.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
 export const SimilarBookDisplay = ({ item }) => {
   const { title, author, image, price, _id } = item
   return (
     <div className="subgenre-row ">
-     
       <div key={_id}>
         <Link to={`/books/${_id}`}>
           <div className="image-wrapper-similar">
@@ -22,23 +20,8 @@ export const SimilarBookDisplay = ({ item }) => {
             <h4 className="price">£ {price}</h4>
           </div>
         </Link>
-        {/* <div className='card-body'>
-            <div className='card-title'> */}
-        <h4>{title}</h4>
-        {/* </div> */}
-        <div className='author'>
-          <h5>{author}</h5>
-          <h4 className="price">£ {price}</h4>
-        </div>
-       
-
-        {/* </div> */}
-
       </div>
-     
     </div>
-    
   )
-
 }
 
