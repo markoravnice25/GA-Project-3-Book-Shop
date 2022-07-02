1d55008615dd001122364b93ed72cf930704ec91
markoravnice25 <markoravnice25@gmail.com>
2022-07-02 20:29:18 +0200

    added BiUser to ReviewDisplay.js

--- client/src/components/ReviewDisplay.js
+++ client/src/components/ReviewDisplay.js
@@ -1,25 +1,35 @@
 import React from 'react'
-import { userIsOwner } from '../helpers/auth'
+import axios from 'axios'
+import { userIsOwner, getTokenFromLocalStorage } from '../helpers/auth'
+import { useParams } from 'react-router-dom'
+import { BiUser } from 'react-icons/bi'
 
-export const ReviewDisplay = ({ review, handleDeleteBtn }) => {
-  console.log('review  -->', review)
+export const ReviewDisplay = ({ review, getBook })  => {
+
+  const { id } = useParams()
+  const handleDeleteBtn = async (e) => {
+    const reviewId = e.target.value
+    console.log('value --->', e.target.value)
+    try {
+      const { data } = await axios.delete(`/api/books/${id}/reviews/${reviewId}`, {
+        headers: {
+          Authorization: `Bearer ${getTokenFromLocalStorage()}`,
+        },
+      })
+
+    } catch (error) {
+      console.log(error.response)
+    }
+    getBook()
+  }
+
   return (
     <div className='review-card'>
+      <h5 className='posted-by'><BiUser />{review.owner.firstName}</h5>
       <h6>&quot;{review.title}&quot;</h6>
-      {/* <p>By {review.owner.firstName} {review.owner.lastName}</p> */}
       <p>{review.text}</p>
-      {userIsOwner(review) ?
-        <div className="owner-buttons mb-4">
-          <button onClick={(e) => handleDeleteBtn(e, review)}>Delete review</button>
-        </div>
-        :
-        <div>
-
-        </div>
-      }
-
-
+      { userIsOwner(review) && <button value={review._id} onClick={handleDeleteBtn}>Delete review</button> }
+      <hr />
     </div>
   )
 }
-
